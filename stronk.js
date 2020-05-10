883459b1a487300c54869f2821c92a79b64cb944
Jacqui Shadforth <pwn@live.com.au>
2020-05-10 17:52:26 +1000

    Clean up code and add extension icon

--- stronk.js
+++ stronk.js
@@ -1,5 +1,5 @@
 const elements = document.querySelectorAll(
-  "h1, h2, h3, h4, h5, h6, p, li, td, caption, span, a"
+  "h1, h2, h3, h4, h5, h6, p, li, td, caption, span"
 );
 
 const findAndReplace = (elements) => {
@@ -9,15 +9,17 @@ const findAndReplace = (elements) => {
 };
 
 const replaceText = (text) => {
-  text.innerHTML = text.innerHTML.replace(/STRONG/g, "STRONK");
-  text.innerHTML = text.innerHTML.replace(/STRONGER/g, "STRONKER");
-  text.innerHTML = text.innerHTML.replace(/STRONGEST/g, "STRONKEST");
-  text.innerHTML = text.innerHTML.replace(/Strong/g, "Stronk");
-  text.innerHTML = text.innerHTML.replace(/Stronger/g, "Stronker");
-  text.innerHTML = text.innerHTML.replace(/Strongest/g, "Stronkest");
-  text.innerHTML = text.innerHTML.replace(/strong/g, "stronk");
-  text.innerHTML = text.innerHTML.replace(/stronger/g, "stronker");
-  text.innerHTML = text.innerHTML.replace(/strongest/g, "stronkest");
+  if (text.innerHTML.match(/strong/i)) {
+    text.innerHTML = text.innerHTML.replace(/STRONG/g, "STRONK");
+    text.innerHTML = text.innerHTML.replace(/STRONGER/g, "STRONKER");
+    text.innerHTML = text.innerHTML.replace(/STRONGEST/g, "STRONKEST");
+    text.innerHTML = text.innerHTML.replace(/Strong/g, "Stronk");
+    text.innerHTML = text.innerHTML.replace(/Stronger/g, "Stronker");
+    text.innerHTML = text.innerHTML.replace(/Strongest/g, "Stronkest");
+    text.innerHTML = text.innerHTML.replace(/strong/g, "stronk");
+    text.innerHTML = text.innerHTML.replace(/stronger/g, "stronker");
+    text.innerHTML = text.innerHTML.replace(/strongest/g, "stronkest");
+  }
 };
 
 findAndReplace(elements);
